f7063be544124b650559a0f9f65b8d26e9d41c64
alejandroaguilarg <alejo852_@hotmail.com>
2021-12-12 10:53:09 -0500

    Microservicios de productos con sus respectivos modelos y repo

---
+++ ms-concesionario-usuarios-admin/src/datasources/mongodb.datasource.ts
@@ -0,0 +1,34 @@
+import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+
+const config = {
+  name: 'mongodb',
+  connector: 'mongodb',
+  url: 'mongodb+srv://usuario-concesionario-g44:C5O1XggJgF43Fo9f@minticg44.dxz5v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  host: 'localhost',
+  port: 27017,
+  user: '',
+  password: '',
+  database: 'usuariosConcesionarioDb',
+  useNewUrlParser: true,
+};
+
+// Observe application's life cycle to disconnect the datasource when
+// application is stopped. This allows the application to be shut down
+// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
+// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
+@lifeCycleObserver('datasource')
+export class MongodbDataSource
+  extends juggler.DataSource
+  implements LifeCycleObserver
+{
+  static dataSourceName = 'mongodb';
+  static readonly defaultConfig = config;
+
+  constructor(
+    @inject('datasources.config.mongodb', {optional: true})
+    dsConfig: object = config,
+  ) {
+    super(dsConfig);
+  }
+}
